38967e226b72f29934b78061d35a4985068ef42f
agathascheidt <agatha.marinda@escola.pr.gov.br>
2025-05-13 14:28:17 +0000

    ok

---
+++ perguntas.js
@@ -0,0 +1,17 @@
+criaCartao(
+    'programação',
+    'o que é HTML?',
+    'linguagem de marcação'
+)
+
+criaCartao(
+    'ingles',
+    'como é dia do pagamento',
+    'pay day'
+)
+
+criaCartao(
+    categoria,
+    pergunta,
+    resposta
+)
